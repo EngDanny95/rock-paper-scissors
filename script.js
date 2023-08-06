661ea3e40202e9b865156cddf4b4606a3c5025e7
Danny <EngDanny95@gmail.com>
2023-08-06 23:57:27 +0300

    build(js): v2 include 5 rounds game & show result

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //Planning Stage:
 //  -The program doesnt have interface at this point
 //  -The user can place their input through a promopt message
-//  -The desired output will show if the user won or lost
+//  -The desired output will show if the user won or lost on console
 //  -To reach that output the input will be compared to the computer input
 //  and show the winner and loser of the game
-//  -The game will be played from the console
 
 //The psudocode would go as follows:
 //  -Computer will have a random built in function input between three 
@@ -13,29 +12,52 @@
 //  -The result will show afterwards by comparison of choices using
 //  if statements
 
-let choice=['rock','paper','scissors'] //create an array with 3 choices
+const choice = ['rock', 'paper', 'scissors'] //create an array with 3 choices
+let x; //the index value to choose from the choice array
+let computerSelection; //the variable that defines the cpu choice
+let playerSelection; //the variable that defines the player choice
+let p1=0;
+let cpu=0;
+let draw=0;
 
-function getComputerChoice(){
-    let x = Math.floor(Math.random()*3) //returns 0,1 or 2
-    return choice[x] //returns a random computer output
+game() //the main function to run
+
+function game() {
+    for (i = 1; i <= 5; i++) { //defines five rounds game
+        function getComputerChoice() { //function to run the computer choice
+            x = Math.floor(Math.random() * 3) //returns 0,1 or 2
+            return choice[x] //returns a random computer output
+        }
+        computerSelection = getComputerChoice() //stores computer choice
+        playerSelection = prompt('rock paper or scissors?').toLowerCase() //player prompt
+        playRound(playerSelection, computerSelection) //function deciding the winner between the players
+    }
+    if (cpu>p1 && cpu>=draw || draw>=p1 && cpu>p1){
+        return console.log('you lost the game')}
+        else if (p1>cpu && p1>=draw || draw>=cpu && p1>cpu)
+        {
+            return console.log('you won the game')
+        }    
+        else {
+                console.log('DRAW')
+            }
+        
+    
 }
 
-let computerSelection=getComputerChoice()
-console.log(computerSelection)
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        draw++;
+        return console.log("draw")
+    }
 
-let playerSelection = prompt('rock paper or scissors?')
+    else if ((playerSelection === choice[0] && computerSelection === choice[1]) ||
+        (playerSelection === choice[1] && computerSelection === choice[2])) {
+        cpu++;
+        return console.log('you lost');
+    }
 
-playRound(playerSelection,computerSelection)
-
-function playRound(playerSelection,computerSelection){
-    if (playerSelection===computerSelection){
-    return console.log("draw")}
-
-    else if((playerSelection===choice[0] && computerSelection===choice[1]) || 
-    (playerSelection===choice[1] && computerSelection===choice[2])){
-    return console.log('you lost')}
-
-    else{
-    return console.log('you won')}
+    else { 
+        p1++;
+        return console.log('you won') }
 }
-  
