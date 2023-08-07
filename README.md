851d02d3f79cd92b0a0960cf1c99d0ef57bfc0aa
Danny <EngDanny95@gmail.com>
2023-08-07 15:26:51 +0300

    build(js): v2.1 fix result of random input & more
    If statement added to show winner if either players win 3 rounds out
    of the 5 rounds first. Bug fixed when input random text to show in
    console as a wrong input.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rock-paper-scissors
 This is my first js project to create a rock paper scissor game. 
+
+At this stage, it works by playing 5 rounds of rock paper scissors through 
+a prompt message displayed on the browsers. The result of the of each round
+will be displayed on the console of the website. If either player or cpu
+reach 3 wins before the end of the 5 rounds, the leader would be declared
+the winner of the game.
+
+The code randomize the cpu choice of rock paper scissors by including them
+in an array of 3 strings. The string get picked up randomly by using Math.random function to pick a number between 0 and 2 to select from the array.
+
+The player picks their choice of rock paper scissors afterwards and the result would be shown in the console log.
--- script.js
+++ script.js
@@ -16,48 +16,60 @@ const choice = ['rock', 'paper', 'scissors'] //create an array with 3 choices
 let x; //the index value to choose from the choice array
 let computerSelection; //the variable that defines the cpu choice
 let playerSelection; //the variable that defines the player choice
-let p1=0;
-let cpu=0;
-let draw=0;
+let p1 = 0; //player variable win count
+let cpu = 0; //cpu variable win count
 
 game() //the main function to run
 
 function game() {
     for (i = 1; i <= 5; i++) { //defines five rounds game
+        if (cpu ===3)  { //display game result if either player wins 3 rounds first
+            i=6;
+            return console.log('YOU LOST 3 ROUNDS!') 
+        }
+        else if (p1 == 3) {
+            i=6;
+            return console.log('YOU WON 3 ROUNDS!')
+        }
         function getComputerChoice() { //function to run the computer choice
             x = Math.floor(Math.random() * 3) //returns 0,1 or 2
             return choice[x] //returns a random computer output
         }
         computerSelection = getComputerChoice() //stores computer choice
+        // console.log(computerSelection) to show the cpu choice
         playerSelection = prompt('rock paper or scissors?').toLowerCase() //player prompt
         playRound(playerSelection, computerSelection) //function deciding the winner between the players
     }
-    if (cpu>p1 && cpu>=draw || draw>=p1 && cpu>p1){
-        return console.log('you lost the game')}
-        else if (p1>cpu && p1>=draw || draw>=cpu && p1>cpu)
-        {
-            return console.log('you won the game')
-        }    
-        else {
-                console.log('DRAW')
-            }
-        
+    if (cpu > p1)  { //display game result message
+        return console.log('YOU LOST!')
+    }
+    else if (cpu < p1) {
+        return console.log('YOU WON!')
+    }
+    else {
+        console.log('DRAW!')
     
+    }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) { //count win, lose rounds and display result per round
     if (playerSelection === computerSelection) {
-        draw++;
         return console.log("draw")
     }
 
     else if ((playerSelection === choice[0] && computerSelection === choice[1]) ||
-        (playerSelection === choice[1] && computerSelection === choice[2])) {
+        (playerSelection === choice[1] && computerSelection === choice[2]) ||
+        (playerSelection == choice[2]) && (computerSelection == choice[0])) {
         cpu++;
         return console.log('you lost');
     }
 
-    else { 
+    else if ((playerSelection === choice[0] && computerSelection === choice[2]) ||
+        (playerSelection == choice[1]) && (computerSelection == choice[0]) ||
+        (playerSelection === choice[2] && computerSelection === choice[1])) {
         p1++;
-        return console.log('you won') }
-}
+        return console.log('you won')
+    }
+    else {
+        return console.log('wrong input')
+    }}
